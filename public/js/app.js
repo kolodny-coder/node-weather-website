c61a74034e4e2d97725482eac84c5fd8a10a3d53
dankolodny <kolodnydan@gmail.com>
2022-04-27 03:06:50 +0300

    Setup app to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading ...'
 
 
-    fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+    fetch(`/weather?address=${address}`).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             messageOne.textContent = data.error
